278d9108261ea6fa52d185b87a8eb6ad93491789
gabrielsoli <gabriels.o527@gmail.com>
2024-09-23 12:40:12 -0300

    adicionando testes de validação da entidade de video

---
+++ encoder/domain/video_test.go
@@ -0,0 +1,29 @@
+package domain_test
+
+import (
+	"encoder/domain"
+	"testing"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidateIfVideoIsEmpty(t *testing.T) {
+	video := domain.NewVideo()
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestIfVideoIDIsNotAUUID(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = uuid.NewV4().String()
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+	require.Nil(t, err)
+}
